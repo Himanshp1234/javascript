fe37dd013be158dea2cffcd8d370456075de0625
Himanshu Pandey <himanshupandey1537@gmail.com>
2026-09-20 16:20:45 +0000

    datatypes

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "hp@6299gmail.com"
+var accountPassword = "12345"
+accountCity = "Arrah"
+let accountState
+
+// accountId = 2 // not allowed
+accountEmail = "himanshupandey1537@gmail.com"
+accountPassword = "21214563"
+accountCity = "patna"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"Use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs,not browser
+
+console.log(3
+    +3) // code readibility should be high
+console.log("Himanshu")
+
+
+let name = "Himanshu" // string datatypes
+let age = 20 // int datatypes
+let isLoggedIn = false //  boolean datatypes
+let state = null
+
+// number <= 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assign value
+// symbol => unique 
+
+// object
+
+console.log(typeof "himanshu")
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
