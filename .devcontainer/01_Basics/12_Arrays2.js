eb620c197775a5a5c11ebc0f36dd09327e31dc95
Himanshu Pandey <himanshupandey1537@gmail.com>
2026-09-25 17:06:56 +0000

    methods of array

---
+++ .devcontainer/01_Basics/12_Arrays2.js
@@ -0,0 +1,28 @@
+//Arrays works as 
+const myArr = {2:4}
+console.log(myArr);
+
+const myArr2 = [2,5,4,6,9,"Himanshu"]
+console.log(myArr2.includes(9));
+console.log(myArr2.indexOf(9));
+
+const newArr = myArr2.join()
+
+console.log(myArr2);
+console.log(typeof myArr2);
+
+// slice , splice
+console.log("A",myArr2);
+
+const myn1 = myArr2.slice(1,3)
+
+console.log(myn1); // output is : [ 5, 4 ]
+B [ 2, 5, 4, 6, 9
+
+
+
+
+
+
+
+ 
