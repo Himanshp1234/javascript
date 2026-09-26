8a4c49385ed50035171e9711a0f7a7f560e9ef93
Himanshu Pandey <himanshupandey1537@gmail.com>
2026-09-26 16:54:14 +0000

    some extra methods of array

--- .devcontainer/01_Basics/12_Arrays2.js
+++ .devcontainer/01_Basics/12_Arrays2.js
@@ -17,8 +17,22 @@ console.log("A",myArr2);
 const myn1 = myArr2.slice(1,3)
 
 console.log(myn1); // output is : [ 5, 4 ]
-B [ 2, 5, 4, 6, 9
+ 
 
+// important methods of arrays
+
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+marvel_heros.push(dc_heros); // push into existing array
+console.log(marvel_heros);
+console.log(marvel_heros[3[1]]);
+
+const allHeros = marvel_heros.concat(dc_heros); // return new heros
+console.log(allHeros);  
+
+const all_new_heros = [...marvel_heros,...dc_heros];
+console.log(all_new_heros); // retun single single element and its also concantes the element        
 
 
 
